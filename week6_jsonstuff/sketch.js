0d5d7e620ecf0b9080fa1f87212c04773b5e0c5f
liferaisin <31519312+liferaisin@users.noreply.github.com>
2018-03-05 22:53:07 -0500

    week6

--- week6_jsonstuff/sketch.js
+++ week6_jsonstuff/sketch.js
@@ -1,67 +1,24 @@
-let paintmarks = [];
-let loadButton;
+var rick;
+// let number = 2;
+function preload() {
+  var url =
+   'https://rickandmortyapi.com/api/character';
+  rick = loadJSON(url);
+}
 
 function setup() {
-  createCanvas(400, 500);
-
-  createButton('save').position(10,10).mousePressed(savePaint);
-
-  loadButton = createFileInput(loadPaint);
-  loadButton.position(width-100, 10);
+  createCanvas(500, 500);
+  var url =
+   'https://rickandmortyapi.com/api/character' + '/?page2';
+  loadJSON(url, randomChar);
 }
 
 function draw() {
-  background(255);
-  for (paintmark of paintmarks) {
-    paintmark.display();
-  }
+  background(200);
 }
 
-function mouseDragged() {
-  paintmarks.push(new PaintMark(createVector(mouseX, mouseY)));
-}
-
-function savePaint() {
-  // console.log(paintmarks[0].toJSON());
-
-  let paintmark_positions = [];
-  for (paintmark of paintmarks) {
-    paintmark_positions.push(paintmark.toJSON());
-  }
-  saveJSON(paintmark_positions, 'paint.json');
-}
-
-function loadPaint(file) {
-   console.log(file);
-  if (file.subtype === 'json') {
-    loadJSON(file.data, parsePaintJSON, errorLoadJSON);
-  }
-}
-
-function parsePaintJSON(data) {
-  for (var property in data) {
-    let position = data[property];
-    console.log(position);
-    paintmarks.push(new PaintMark(createVector(position.x, position.y)));
-  }
-}
-
-function errorLoadJSON(error) {
-  console.log(error);
-}
-
-class PaintMark {
-  constructor(position) {
-    this.position = position;
-  }
-
-  display() {
-    noStroke();
-    fill(0, 0, 0, 64);
-    ellipse(this.position.x, this.position.y, 10, 10);
-  }
-
-  toJSON() {
-    return { "x": this.position.x, "y": this.position.y };
-  }
+function randomChar(rick) {
+  let charname = results["name"];
+  textAlign(CENTER);
+  text(charname, 0, height - 30, width, 30);
 }
