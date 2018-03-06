4ef48d6a434353413f595abf193e6ebd49c25103
liferaisin <31519312+liferaisin@users.noreply.github.com>
2018-03-06 09:52:17 -0500

    week 6

--- week6_jsonstuff/sketch.js
+++ week6_jsonstuff/sketch.js
@@ -1,24 +1,46 @@
+// Eva Serrano Reisner
+// 3.5.18
+
+// I ACTUALLY SUCCEEDED IN WHAT I WANTED TO DO
+// used the Rick and Morty API to make a random character generator
+// I know that's like super simple sorry, but it was weird trying to figure out
+// the exact wording needed in different APIs
+// I was gonna do pokemon, but I couldn't even figure out
+// how to get the JSON from that one.
+// so overall even though this is small and silly I'm  happy haha
+
 var rick;
-// let number = 2;
+let picker;
+let pick;
+
 function preload() {
+  picker = random(394);
+  pick = picker.toFixed(0);
   var url =
-   'https://rickandmortyapi.com/api/character';
+   'https://rickandmortyapi.com/api/character/' + pick;
   rick = loadJSON(url);
 }
 
 function setup() {
+  noLoop();
   createCanvas(500, 500);
+  picker = random(394);
+  pick = picker.toFixed(0);
   var url =
-   'https://rickandmortyapi.com/api/character' + '/?page2';
+   'https://rickandmortyapi.com/api/character/' + pick;
   loadJSON(url, randomChar);
 }
 
 function draw() {
-  background(200);
+  background(random(255), random(255), random(255));
+  console.log(pick);
+  textAlign(CENTER, CENTER);
+  // text("today you are " + charname, 0, height/2, width, 30);
 }
 
 function randomChar(rick) {
-  let charname = results["name"];
-  textAlign(CENTER);
-  text(charname, 0, height - 30, width, 30);
+  let charname = rick.name;
+  textSize(20);
+  textAlign(CENTER, CENTER);
+  text("today you are " + charname, height/2, width/2);
 }
